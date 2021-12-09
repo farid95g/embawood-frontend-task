6074e3465f1db9e802bda78d3d5f2c9c450307c4
farid95g <f95guluzade@gmail.com>
2021-12-10 02:37:38 +0400

    complex pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -5,17 +5,20 @@ export const CustomPagination = ({
   total,
   booksPerPage,
   page,
-  getCurrentPage
+  getCurrentPage,
 }) => {
-  
   let active = Number(page);
   let items = [];
   let lastPageNumber = Math.ceil(Number(total) / booksPerPage);
   if (isFinite(lastPageNumber)) {
-    console.log(lastPageNumber)
+    console.log(lastPageNumber);
     for (let number = 1; number <= lastPageNumber; number++) {
       items.push(
-        <Pagination.Item key={number} active={number === active} onClick={() => getCurrentPage(number)}>
+        <Pagination.Item
+          key={number}
+          active={number === active}
+          onClick={() => getCurrentPage(number)}
+        >
           {number}
         </Pagination.Item>
       );
@@ -24,7 +27,27 @@ export const CustomPagination = ({
 
   return (
     <div className="d-flex justify-content-center mt-3">
-      <Pagination>{items}</Pagination>
+      <Pagination>
+        <Pagination.First onClick={() => getCurrentPage(1)} />
+        <Pagination.Prev onClick={() => getCurrentPage(active - 1)} />
+        {items.map((item, index) => {
+          console.log(item, index);
+          if (index < 2 || index > lastPageNumber - 3) {
+            return item;
+          } else {
+            if (index > active - 2 && index < active + 2) {
+              return item;
+            } else if (index > active - 3 && index < active + 3) {
+              return item;
+            } else if (index === active + 3 || index === active - 3) {
+              return <Pagination.Ellipsis />;
+            }
+          }
+          return null;
+        })}
+        <Pagination.Next onClick={() => getCurrentPage(active + 1)} />
+        <Pagination.Last onClick={() => getCurrentPage(lastPageNumber)} />
+      </Pagination>
     </div>
   );
 };
